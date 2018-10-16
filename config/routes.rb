8ae4424001a02d9b7d8092926e2f42124a93d585
脇田有人 <alto@wakitanoMacbookPro.local>
2018-10-16 22:55:07 +0900

    ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/authentication'
+  root 'home#index'
+  get  '/home', to: 'home#index'
+  get '/login', to: 'home#authentication' 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
