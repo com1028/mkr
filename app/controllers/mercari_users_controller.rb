2f5248035f6a54c46d866d3a74b791b8654bd7ca
脇田有人 <alto@wakitanoMacbookPro.local>
2018-10-19 14:47:07 +0900

    「メルカリアカウント」を作成ボタンを押下した際のPOST先（def create）の中身を作成中

--- app/controllers/mercari_users_controller.rb
+++ app/controllers/mercari_users_controller.rb
@@ -6,4 +6,25 @@ class MercariUsersController < ApplicationController
     def new
         @mercari_user = MercariUser.new
     end
+
+    def create
+        @mercari_user = MercariUser.new(mercari_user_params)
+        if @mercari_user.save
+        # ユーザの作成に成功した場合
+        log_in @mercari_user
+        flash[:success] = "ユーザ登録が完了しました"
+        redirect_to @mercari_user
+
+        else
+        # ユーザの作成に失敗した場合
+
+    end
+        # redirect_to new_mercari_user_path
+    end
+
+      # メルカリアカウント情報のstrongパラメータの設定
+    private def mercari_user_params
+        params.require(:mercari_user).permit(:name, :email, :password)
+    end
+
 end
