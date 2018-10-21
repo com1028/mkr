2f428251b4e59002fab1f7380d86286fbcc49069
脇田有人 <w.alto0903@gmail.com>
2018-10-22 07:16:42 +0900

    再描画の再の画像ファイルのサイズを拡大

--- app/controllers/mercari_users_controller.rb
+++ app/controllers/mercari_users_controller.rb
@@ -26,7 +26,9 @@ class MercariUsersController < ApplicationController
 
     def create
         @mercari_user = current_user.mercari_users.new(mercari_user_params)
-        @mercari_user.setMercariToken()
+        # @mercari_user.setMercariToken()
+        @mercari_user.access_token = "aaa"
+        @mercari_user.global_access_token = "bbb"
          if @mercari_user.save
             # メルカリアカウントの作成に成功した場合
             flash[:success] = "メルカリアカウントの登録が完了しました"
