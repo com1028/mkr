a31264fff2662bb1ff4c91331b6fd8fae389c032
脇田有人 <w.alto0903@gmail.com>
2018-10-22 04:38:38 +0900

    メルカリアカウントの更新は、ログイン認証をできるか不確定なので、「新規でアカウントを作成」ページへのリンクを作成

--- app/controllers/mercari_users_controller.rb
+++ app/controllers/mercari_users_controller.rb
@@ -12,18 +12,19 @@ class MercariUsersController < ApplicationController
     end
 
     def update
-        after_delete_user = current_user.mercari_users.find_by(id:params[:id])
-        @mercari_user = current_user.mercari_users.new(mercari_user_params)
-        @mercari_user.setMercariToken()
-        if @mercari_user.save
-            # メルカリアカウントの作成に成功した場合    
-            after_delete_user.delete
-            flash[:success] = "メルカリアカウントの更新が完了しました"
-            redirect_to mercari_users_path
-        else
-            # メルカリアカウントの作成に失敗した場合
-            render 'new'
-        end
+        # after_delete_user = current_user.mercari_users.find_by(id:params[:id])
+        # @mercari_user = current_user.mercari_users.new(mercari_user_params)
+        # # binding.pry
+        # # @mercari_user.setMercariToken()
+        # if @mercari_user.save
+        #     # メルカリアカウントの作成に成功した場合    
+        #     after_delete_user.delete
+        #     flash[:success] = "メルカリアカウントの更新が完了しました"
+        #     redirect_to mercari_users_path
+        # else
+        #     # メルカリアカウントの作成に失敗した場合
+        #     render 'new'
+        # end
     end
 
     def create
