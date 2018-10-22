69e956f5ccca52c5063f13013074d5bca2486467
脇田有人 <w.alto0903@gmail.com>
2018-10-23 04:45:50 +0900

    「商品データ」に関するグローバル変数を定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,32 +14,26 @@ class Item < ApplicationRecord
   validates :auto_exhibit_flag, presence: true
 
   def getItemCondition
-    options = ["新品，未使用","未使用に近い","目立った傷や汚れなし",
-      "やや傷や汚れあり","傷や汚れあり","全体的に状態が悪い"]
+    options = ItemConstant::ITEM_CONDITION_OPTIONS
     return options[self.item_condition-1]
   end
 
   def getShippingDuration
-    options = ["1〜2日で発送","2〜3日で発送","4〜7日で発送"]
+    options = ItemConstant::SHIPPING_DURATION_OPTIONS
     return options[self.shipping_duration-1]
   end
 
   def getShippingFromArea
-    options = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県",
-			"埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県",
-			"岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県",
-			"鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県",
-      "佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
-      
+    options = ItemConstant::SHIPPING_FROM_AREA_OPTIONS
       return options[self.shipping_from_area-1]
   end
 
   # 自動出品をするかを返す
   def getAutoExhibitFlag
     if self.auto_exhibit_flag?
-      return 'する'
+      return ItemConstant::AUTO_EXHIBIT_FLAG_OPTIONS[0]
     else
-      return 'しない'
+      return ItemConstant::AUTO_EXHIBIT_FLAG_OPTIONS[1]
     end
   end
 
