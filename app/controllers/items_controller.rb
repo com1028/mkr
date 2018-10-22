b1319885e17d10dbc058b6cda33f667cbd157a39
脇田有人 <w.alto0903@gmail.com>
2018-10-23 03:11:16 +0900

    メルカリ出品用の商品テーブルを作成 && 一部、Getterの設置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
     def index
         @mercari_user = MercariUser.find_by(id: params['mercari_user_id'])
+        @items = @mercari_user.items.all
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,27 @@ class Item < ApplicationRecord
   validates :item_condition, presence: true
    # メルカリで出品できる範囲が、1,000円から999,999円の範囲？
   validates :price, presence: true, inclusion: { in: 1000..999999 }
-  validates :shipping_from_area, presence: true
+  validates :shipping_from_area, presence: true, inclusion: { in: 1..47 }
   validates :contents, length: { maximum: 1000 }
   validates :auto_exhibit_flag, presence: true
+
+  def getShippingFromArea
+    options = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県",
+			"埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県",
+			"岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県",
+			"鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県",
+      "佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
+      
+      return options[self.shipping_from_area-1]
+  end
+
+  # 自動出品をするかを返す
+  def getAutoExhibitFlag
+    if self.auto_exhibit_flag?
+      return 'する'
+    else
+      return 'しない'
+    end
+  end
+
 end
