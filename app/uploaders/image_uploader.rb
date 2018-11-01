c647a6b61ba6d15ad08fd6f74adb47f8cfde1336
脇田有人 <w.alto0903@gmail.com>
2018-11-01 22:59:06 +0900

    不要なコメントを削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,14 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "#{model.user.class.to_s.underscore}/#{model.user.id}/#{model.class.to_s.underscore}/icon/#{model.id}"
   end
@@ -21,19 +13,10 @@ class ImageUploader < CarrierWave::Uploader::Base
  
   # 保存形式をJPGにする
   process :convert => 'jpg'
- 
-  # サムネイルを生成する設定
-  # version :thumb do
-  #   process :resize_to_limit => [300, 300]
-  # end
    
   version :thumb100 do
     process :resize_to_limit => [100, 100]
   end
- 
-  # version :thumb30 do
-  #   process :resize_to_limit => [30, 30]
-  # end
  
   # jpg,jpeg,gif,pngしか受け付けない
   def extension_white_list
