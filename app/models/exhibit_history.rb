58767be7e3fcc6f81b6284bbcca7ba24eb938d35
脇田有人 <w.alto0903@gmail.com>
2018-11-06 00:18:07 +0900

    出品時に取得したmercari_item_tokenをモデルに格納

--- app/models/exhibit_history.rb
+++ app/models/exhibit_history.rb
@@ -2,4 +2,6 @@ class ExhibitHistory < ApplicationRecord
 	belongs_to :user, required: true
 	belongs_to :mercari_user, required: true
 	belongs_to :item, required: true
+
+	validates :mercari_item_token, presence: true, uniqueness: true
 end
