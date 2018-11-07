903579ab33976c8cd2bb4a9fb7e4e52c030b06f9
脇田有人 <w.alto0903@gmail.com>
2018-11-07 11:13:49 +0900

    javaコマンドの説明を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -73,6 +73,7 @@ class ItemsController < ApplicationController
     def exhibit
         @item = Item.find_by(id: params['item_id'])
         @mercari_user = @item.mercari_user
+        # メルカリへの出品はJavaのAPIを通して行うので、Linux上でjavaコマンドを生成して実行する
         cmd = "java -jar #{APIConstant::API_PATH}/exhibitAPI.jar #{@item.mercari_user.global_access_token} #{@item.mercari_user.access_token} #{@item.getImageFullPath(@item.image1.to_s)} #{@item.getImageFullPath(@item.image2.to_s)} #{@item.getImageFullPath(@item.image3.to_s)} #{@item.getImageFullPath(@item.image4.to_s)} '#{@item.item_name}' '#{@item.contents}' #{@item.category} #{@item.item_condition} #{@item.shippingPayer} #{@item.shippingMethod} #{@item.shipping_from_area} #{@item.shipping_duration} #{@item.price}"
         result = `#{cmd}`
         if result.start_with?("m") && !result.include?("\n")
