51f9b8814c81f4d050e91e555aae1975279d8741
脇田有人 <w.alto0903@gmail.com>
2018-11-21 22:26:38 +0900

    タスクで自動出品処理を実現

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -108,40 +108,6 @@ class ItemsController < ApplicationController
       auto_exhibit_user.update(in_progress: false) if auto_exhibit_user.present?
     end
 
-    # 自動出品処理
-    def auto_exhibit
-      mercari_users = MercariUser.all_in_progress_user
-      mercari_users.each do |mercari_user|
-        final_auto_exhibit_count = Item.final_auto_exhibit_count(mercari_user.id)
-        # まだ自動出品をしていないとき
-        if final_auto_exhibit_count == 0
-          # 最終出品商品は、商品レコードの最後の商品とみなす
-          last_exhibit_item = mercari_user.items.last_exhibit_item(mercari_user.id).last
-        # 既に自動出品をしているとき
-        else final_auto_exhibit_count == 1
-          last_exhibit_item = mercari_user.items.last_exhibit_item(mercari_user.id)
-        end
-        if mercari_user.items.auto_exhibit_count_by_mercari_user(mercari_user.id) == 0
-          next_exhibit_item = nil
-        else
-          next_exhibit_item = mercari_user.items.next_exhibit_item(mercari_user.id, last_exhibit_item)
-
-          # 前回の出品から出品時間(デフォルトは2時間)が経過している場合
-          exhibit_interval = mercari_user.exhibit_interval
-          exhibit_interval = 2 if exhibit_interval.nil?
-          if Time.now >= last_exhibit_item.last_auto_exhibit_date.to_time + exhibit_interval.hour
-            # # 過去の同じ商品の出品でコメントのあった商品はメルカリ上から削除
-            next_exhibit_item.deleteIfExistComment
-            # # 出品処理
-            next_exhibit_item.exhibit
-            # 最終自動出品日時を更新
-            last_exhibit_item.update(last_auto_exhibit_date: nil)
-            next_exhibit_item.update(last_auto_exhibit_date: Time.now.to_s(:db))
-          end
-        end
-      end
-    end
-
     private
 
     def item_params
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,8 @@
+set :output, 'log/crontab.log'
+ENV['RAILS_ENV'] ||= 'development'
+set :environment, ENV['RAILS_ENV']
+
 # 1分毎に回す
 every 1.minute do
-  command "open /Users/alto/Desktop"
+  rake 'auto_exhibit:auto_exhibit'
 end
